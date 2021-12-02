60ff07beebfab894836d26ae8c084c5c004b2846
Nixon1097 <nixonandrewparra@gmail.com>
2021-12-01 20:43:30 -0400

    finish homework 1

---
+++ 02-JS-I/homework/public class ArrayDeNumeros2.js
@@ -0,0 +1,13 @@
+public class=" ArrayDeNumeros2 "
+{
+    public static void main(String[] args)
+    {
+        int i;
+        int[] numeros = { 23, 46, 520, 8, 17 };
+
+        for(i=0; i<=4; i++)
+        {
+            System.out.println(numeros[i]);
+        }
+    }
+};
